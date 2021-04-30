4d72039f439e1fdff56770fb4c3d54146fad0bb2
Saad Hassan <saad.haxxan786@gmail.com>
2021-04-30 15:11:20 +0500

    added urls and home page views

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Report(models.Model):
+    name = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='reports',blank=True)
+    remarks = models.TextField()
+    author = models.ForeignKey("profiles.Profile",on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=False)
+    
+    def __str__(self):
+        return str(self.name)
+    
