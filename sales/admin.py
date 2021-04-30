13945be241247abe87bdf2dd04d18eabbb3a4e79
Saad Hassan <saad.haxxan786@gmail.com>
2021-04-30 14:35:54 +0500

    added models in sales app

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Sale,CSV,Position
 
-# Register your models here.
+admin.site.register(Position)
+admin.site.register(CSV)
+admin.site.register(Sale)
